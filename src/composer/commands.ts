8328d9f428ec944bec7e01fc112b96f3af2acda7
Adrián Hojas Moré <hojas.adrian@outlook.com>
2024-07-11 11:04:22 -0400

    add clear data command

--- src/composer/commands.ts
+++ src/composer/commands.ts
@@ -2,10 +2,11 @@ import { Composer } from "../deps.ts";
 import MyContext from "../helpers/context.ts";
 import isAdmin from "../middlewares/is_admin.ts";
 import onPokemonCommandHandler from "../handlers/on_add_pokemon_command_handler.ts";
+import onClearDataCommandHandler from "../handlers/on_clear_data_command_handler.ts";
 
 const composer = new Composer<MyContext>();
 
 composer.command("addpokemon", isAdmin, onPokemonCommandHandler);
-composer.command("info", isAdmin, async (ctx) => await ctx.reply("pong"));
+composer.command("cleardata", isAdmin, onClearDataCommandHandler);
 
 export default composer;
---
+++ src/handlers/on_clear_data_command_handler.ts
@@ -0,0 +1,9 @@
+import MyContext from "../helpers/context.ts";
+
+import { deleteDatatoSave } from "../helpers/session_actions.ts";
+
+export default async (ctx: MyContext) => {
+  deleteDatatoSave(ctx);
+
+  return await ctx.reply("datos borrados");
+};
