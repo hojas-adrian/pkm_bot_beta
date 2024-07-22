a7507243e84e9db1d6ff31c69d7e14f2754120fd
Adrián Hojas Moré <hojas.adrian@outlook.com>
2024-07-21 23:57:48 -0400

    v0.0.1

--- src/middlewares/random.ts
+++ src/middlewares/random.ts
@@ -2,8 +2,6 @@ import { NextFunction } from "../deps.ts";
 import MyContext from "../helpers/context.ts";
 
 export default async (ctx: MyContext, next: NextFunction) => {
-  ctx.react("☃");
-
   if (Math.random() < 0.25) {
     await next();
   }
